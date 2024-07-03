eec03bb3733111361ab54b62978ad86b36a1088a
Kloe <115853526+Kloepetr@users.noreply.github.com>
2024-07-03 09:31:14 -0400

    Update projects.js
    same commands

--- projects.js
+++ projects.js
@@ -11,15 +11,3 @@ function toggleParagraph(paragraphId) {
     }
   }
   
-  function toggleParagraph(paragraphId) {
-    const paragraph = document.getElementById(paragraphId);
-    const button = document.querySelector(`button[onclick="toggleParagraph('${paragraphId}')"]`);
-    
-    if (paragraph.style.display === "none") {
-      paragraph.style.display = "block";
-      button.innerText = "Hide project objectives";
-    } else {
-      paragraph.style.display = "none";
-      button.innerText = "Project objectives";
-    }
-  }
